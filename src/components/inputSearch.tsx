15cdba52c89d72d29fb5f3cfcc3b10a7b1a28665
Daniel <danielrgb2@gmail.com>
2023-05-03 15:27:15 -0300

    feat: header searchBox

--- src/components/inputSearch.tsx
+++ src/components/inputSearch.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { Flex, Input, Icon } from '@chakra-ui/react'
+import { useRef } from "react";
+import { RiSearchLine } from "react-icons/ri";
+
 
 export const InputSearch = () => {
+
+    const searchInputRef = useRef<HTMLInputElement>(null)
+
     return (
-        <div>inputSearch</div>
+
+        <Flex
+            as="label"
+            flex='1'
+            py='3'
+            px='5'
+            ml='6'
+            maxWidth={400}
+            alignSelf='center'
+            position='relative'
+            boxShadow={'base'}
+            border={'1px solid #E1E6DD'}
+            borderRadius={'3.5em'}
+        >
+
+            <Input
+                type='text'
+                variant='unstyled'
+                px='4'
+                mr='4'
+                ref={searchInputRef}
+                placeholder='Pesquisar pokemon'
+
+                fontFamily={'Lato'}
+                fontWeight={'700'}
+                fontSize={'17px'}
+                opacity={'.5'}
+                colorScheme='blackAlpha'
+            />
+
+            <Icon as={RiSearchLine} opacity={.5} fontSize='20' />
+        </Flex>
+
+
     )
 }
--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import { Box, Text, Button, Flex, Image, Input, InputGroup, Stack, InputLeftElement, InputRightElement } from '@chakra-ui/react'
+import { InputSearch } from '../components/inputSearch'
 
 export const Dashboard = () => {
   return (
-    <Flex >
-        <Text>oi</Text>
+    <Flex
+      w='100vw'
+      h='100vh'
+      flexDir={'column'} >
+      <Flex
+        justifyContent={'center'}
+        w='100%'
+        h='4%'
+        m='10px 0px'>
+        <InputSearch />
+      </Flex>
+
+      <Flex
+        w='100%'
+        h='96%'>
+
+      </Flex>
+
     </Flex>
   )
 }
