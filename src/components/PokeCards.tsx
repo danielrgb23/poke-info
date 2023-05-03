de64a5a59d416b4fd97925af6662b3f504e61fce
Daniel <danielrgb2@gmail.com>
2023-05-03 17:52:10 -0300

    feat: integração a API

--- src/components/PokeCards.tsx
+++ src/components/PokeCards.tsx
@@ -1,11 +1,33 @@
 import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import image from '../assets/background/pokeballCard.png'
+import axios from 'axios'
 
-export const PokeCards = () => {
+interface ItypesPokemon {
+    name: string
+    url: string
+}
 
-    const typeColors = {
+interface ItypePokemon {
+    type: ItypesPokemon
+}
+
+interface IPokemonResponse {
+    order: number,
+    name: string,
+    types: ItypePokemon[],
+    id: number,
+    weight: number
+}
+
+export const PokeCards = ({ itens }: any) => {
+
+    const [pokemon, setPokemon] = useState<IPokemonResponse>()
+    const [loading, setLoading] = useState(false)
+    const [opacityValue, setOpacityValue] = useState(0)
+
+    const typeColors: any = {
         "grass": "#78C850",
         "fire": "#F08030",
         "water": "#6890F0",
@@ -15,6 +37,45 @@ export const PokeCards = () => {
         "phantom": "#705898",
         "normal": "#A8A878"
     }
+
+    const verifyColor = () => {
+        const types = pokemon!?.types.length > 0 ? pokemon?.types[0].type.name : null
+        return typeColors[types || "normal"];
+    }
+
+    // console.log('euu',pokemon)
+
+    useEffect(() => {
+        setLoading(true)
+        setOpacityValue(0.3) // definir opacidade inicial para o efeito
+
+        axios.get(`${itens.url}`)
+            .then((res) => {
+                console.log(res)
+                const response = res.data;
+                const pokemonResponse = {
+                    order: response.order,
+                    name: response.name,
+                    types: response.types,
+                    id: response.id,
+                    weight: response.weight
+                }
+                setLoading(false)
+                setPokemon(pokemonResponse)
+            }).catch((err) => {
+                setLoading(false)
+            })
+    }, [])
+
+    useEffect(() => {
+        if (loading) {
+            // definir a animação de opacidade enquanto carrega
+            const interval = setInterval(() => {
+                setOpacityValue((prev) => prev === 0.25 ? 0.3 : 0.25)
+            }, 500)
+            return () => clearInterval(interval)
+        }
+    }, [loading])
 
     return (
         <Card>
@@ -31,16 +92,16 @@ export const PokeCards = () => {
                         fontSize={'17px'}
                         // opacity={'.5'}
                         colorScheme='blackAlpha'
-                    >#3</Text>
+                    >#{pokemon?.order}</Text>
                     <Text
                         fontFamily={'Poppins'}
                         fontWeight={'700'}
                         fontSize={'20px'}
                         colorScheme='blackAlpha'
-                    > &nbsp;Ivysaur</Text>
+                    > &nbsp;{pokemon?.name}</Text>
                 </Heading>
                 <Flex
-                    bg='#78C850'
+                    bg={verifyColor()}
                     boxShadow={'base'}
                     borderRadius={'3em'}
                     p='0px 15px'
@@ -53,7 +114,7 @@ export const PokeCards = () => {
                     bgPosition={'top right'}
                     justifyContent={'center'}>
                     <Image
-                        src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png'
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`}
                         alt='Green double couch with wooden legs'
                         borderRadius='lg'
                     />
@@ -66,36 +127,26 @@ export const PokeCards = () => {
                         flexDir={'row'}
                         // gap='8px'
                         w='100%'>
-                        <Text
-                            display={'flex'}
-                            borderRadius={'10px'}
-                            // w='100%'
-                            p='0px 10px'
-                            bg='#78C850'
-                            alignItems={'center'}
-                            justifyContent={'center'}
+                        {
+                            pokemon?.types?.map(({ type, index }: any) => (
+                                <Text
+                                    key={index}
+                                    display={'flex'}
+                                    borderRadius={'10px'}
+                                    // w='100%'
+                                    p='0px 10px'
+                                    bg='#78C850'
+                                    alignItems={'center'}
+                                    justifyContent={'center'}
 
-                            color={'white'}
-                            fontFamily={'Poppins'}
-                            fontWeight={'400'}
-                            fontSize={'14px'}>
-                            Grass
-                        </Text>
-                        <Text
-                            display={'flex'}
-                            borderRadius={'10px'}
-                            // w='100%'
-                            p='0px 10px'
-                            bg='#78C850'
-                            alignItems={'center'}
-                            justifyContent={'center'}
-
-                            color={'white'}
-                            fontFamily={'Poppins'}
-                            fontWeight={'400'}
-                            fontSize={'14px'}>
-                            Poison
-                        </Text>
+                                    color={'white'}
+                                    fontFamily={'Poppins'}
+                                    fontWeight={'400'}
+                                    fontSize={'14px'}>
+                                    {type.name}
+                                </Text>
+                            ))
+                        }
                     </Flex>
                     <Flex
                         alignItems={'center'}
@@ -116,7 +167,7 @@ export const PokeCards = () => {
                             fontFamily={'Poppins'}
                             fontWeight={'400'}
                             fontSize={'14px'}>
-                            40Kg
+                            {pokemon?.weight}Kg
                         </Text>
                     </Flex>
                 </Stack>
--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { Box, Text, Button, Flex, Image, Input, InputGroup, Stack, InputLeftElement, InputRightElement, WrapItem, Wrap } from '@chakra-ui/react'
+import axios from 'axios'
+
 import { SearchBox } from '../components/searchBox'
 import { PokeCards } from '../components/PokeCards'
 
 export const Dashboard = () => {
+
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => {
+        setPokemons(res.data.results)
+      }).catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+  // console.log(pokemons)
+
   return (
     <Flex
       w='100vw'
@@ -13,24 +29,27 @@ export const Dashboard = () => {
         justifyContent={'center'}
         w='100%'
         h='4%'
-        m='10px 0px'>
+        m='3em 0px'>
         <SearchBox />
       </Flex>
 
       <Flex
         p='20px 20px'
         w='100%'
+        justifyContent={'center'}
       >
-        <Wrap w='100%'  spacing='20px'>
-          <WrapItem w='250px' h='410px' >
-            <PokeCards/>
-          </WrapItem>
-          <WrapItem w='250px'>
-            <PokeCards />
-          </WrapItem>
-          <WrapItem w='250px'>
-            <PokeCards />
-          </WrapItem>
+        <Wrap w='100%' spacing='50px'>
+          {
+            pokemons.map((pokemon) => {
+              return (
+                <WrapItem w='250px' h='420px'>
+                  <PokeCards
+                    itens={pokemon}
+                  />
+                </WrapItem>
+              )
+            })
+          }
         </Wrap>
       </Flex>
 
